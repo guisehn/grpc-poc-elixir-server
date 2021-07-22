1cde2da7abf9fc59acefec7de2ac5029b6e2c808
Guilherme Sehn <guilherme.sehn@therealreal.com>
2021-07-22 15:51:38 -0300

    add error example

--- lib/sample_app.ex
+++ lib/sample_app.ex
@@ -9,6 +9,10 @@ defmodule SampleApp.Hello.Server do
   use GRPC.Server, service: GrpcPoc.HelloService.Service
 
   @spec hello(GrpcPoc.HelloRequest.t, GRPC.Server.Stream.t) :: GrpcPoc.HelloResponse.t
+  def hello(%GrpcPoc.HelloRequest{name: "fail_test"}, _stream) do
+    raise GRPC.RPCError, status: GRPC.Status.not_found(), message: "not found"
+  end
+
   def hello(%GrpcPoc.HelloRequest{name: name}, _stream) do
     %GrpcPoc.HelloResponse{message: "Hi #{name}!"}
   end
