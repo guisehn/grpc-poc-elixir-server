a258023ac98a82a1de25d0ccf3c9eff7e3d572ba
Guilherme Sehn <guilherme.sehn@therealreal.com>
2021-07-22 13:18:14 -0300

    add typespec

--- lib/sample_app.ex
+++ lib/sample_app.ex
@@ -8,6 +8,7 @@ end
 defmodule SampleApp.Hello.Server do
   use GRPC.Server, service: GrpcPoc.HelloService.Service
 
+  @spec hello(GrpcPoc.HelloRequest.t, GRPC.Server.Stream.t) :: GrpcPoc.HelloResponse.t
   def hello(%GrpcPoc.HelloRequest{name: name}, _stream) do
     %GrpcPoc.HelloResponse{message: "Hi #{name}!"}
   end
